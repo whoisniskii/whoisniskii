7152dcc05fb3194b34fd195b433a2e8cb1ae282f
Nicolas Ribeiro <nicolas.ribeiro.s@outlook.com>
2022-06-18 20:48:51 -0300

    chanel

--- src/components/discord/Avatar.tsx
+++ src/components/discord/Avatar.tsx
@@ -6,17 +6,15 @@ function DiscordAvatar() {
   const discordUserData = useLanyard('847865068657836033');
 
   const userData = useMemo(() => {
-    return discordUserData?.data;
+    return discordUserData;
   }, [discordUserData]);
 
   return (
-    <div className="min-w-[5rem] relative inline-block">
-      <img
-        className="w-28 h-28 rounded-full mb-4 sm:mb-0 sm:mr-4"
-        src={`https://cdn.discordapp.com/avatars/${userData?.discord_user.id}/${userData?.discord_user.avatar}${userData?.discord_user.avatar.startsWith('a_') ? '.gif' : '.png'}?size=128`}
-        alt={`${userData?.discord_user.username}#${userData?.discord_user.discriminator}`}
-      />
-      <DiscordStatus />
+    <div>
+      <div className="min-w-[5rem] relative inline-block">
+        <img className="w-28 h-28 rounded-full mb-4 sm:mb-0 sm:mr-4" src="src/assets/avatar.png" alt={`${userData?.data?.discord_user.username}#${userData?.data?.discord_user.discriminator}`} />
+        <DiscordStatus />
+      </div>
     </div>
   );
 }
