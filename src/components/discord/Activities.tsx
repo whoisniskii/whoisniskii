fa4497abb40822b99168c0e928402a2ad6f55d4c
Nicolas Ribeiro <nicolas.ribeiro.s@outlook.com>
2022-06-19 17:20:11 -0300

    la la la

--- src/components/discord/Activities.tsx
+++ src/components/discord/Activities.tsx
@@ -1,10 +1,10 @@
-import { useLanyard } from 'use-lanyard';
+import { useLanyardWs } from 'use-lanyard';
 import { SpotifyActivity } from './activity/Spotify';
 
 function DiscordActivities() {
-  const discordUserData = useLanyard('847865068657836033');
+  const discordUserData = useLanyardWs('847865068657836033');
 
-  return <div>{discordUserData?.data?.listening_to_spotify && <SpotifyActivity />}</div>;
+  return <div>{discordUserData?.listening_to_spotify && <SpotifyActivity />}</div>;
 }
 
 export { DiscordActivities };
--- src/components/discord/Avatar.tsx
+++ src/components/discord/Avatar.tsx
@@ -12,13 +12,18 @@ function DiscordAvatar() {
   return (
     <div>
       <div className="min-w-[5rem] relative inline-block">
-        <img
-          className="w-28 h-28 rounded-full mb-4 sm:mb-0 sm:mr-4"
-          src={`https://cdn.discordapp.com/avatars/${userData?.data?.discord_user.id}/${userData?.data?.discord_user.avatar}${
-            userData?.data?.discord_user.avatar.startsWith('a_') ? '.gif' : '.png'
-          }?size=128`}
-          alt={`${userData?.data?.discord_user.username}#${userData?.data?.discord_user.discriminator}`}
-        />
+        {userData.data?.discord_user.avatar ? (
+          <img
+            className="w-28 h-28 rounded-full mb-4 sm:mb-0 sm:mr-4"
+            src={`https://cdn.discordapp.com/avatars/${userData?.data?.discord_user.id}/${userData?.data?.discord_user.avatar}${
+              userData?.data?.discord_user.avatar.startsWith('a_') ? '.gif' : '.png'
+            }?size=128`}
+            alt={`${userData?.data?.discord_user.username}#${userData?.data?.discord_user.discriminator}`}
+          />
+        ) : (
+          <img className="w-28 h-28 rounded-full mb-4 sm:mb-0 sm:mr-4" src="src/assets/avatar.png" alt={`${userData?.data?.discord_user.username}#${userData?.data?.discord_user.discriminator}`} />
+        )}
+
         <DiscordStatus />
       </div>
     </div>
